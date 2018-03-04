13abf1e311ebc2adab25c3f5f3a0b9e9866539bc
Lev Khotov <lfyuomr-gylo@yandex.ru>
2018-03-04 22:22:40 +0300

    This piece of shit behaves randomly. Fuck you python

--- src/Repl.py
+++ src/Repl.py
@@ -1,8 +1,12 @@
-#! /usr/bin/env python3.5
+# from src import EthereumClient
+
 
 class Repl:
 
-    def __init__(self) -> None:
+    def __init__(self, eth_client) -> None:
+        self._eth_client = eth_client
+
+        # constants:
         position_format_description = 'The position is specified as <COLUMN><LINE>, e.g. H8.'
         self._commands = {
             # command -> (handler_function, arguments_description, help description)
@@ -57,8 +61,15 @@ class Repl:
                 'print all available commands'
             )
         }
+        self._column_letters = dict(zip((chr(c) for c in range(ord('A'), ord('O') + 1)), range(15)))
 
     def handle_user_input(self, line: str):
+        try:
+            self._handle_user_input(line)
+        except ValueError as e:
+            print("Failed: {}".format(str(e)))
+
+    def _handle_user_input(self, line: str):
         line = line.strip()
         for command, (handler, _, _) in self._commands.items():
             if line.startswith(command):
@@ -77,15 +88,30 @@ class Repl:
         pass
 
     def _on_move(self, move: str):
+        line, column = self._parse_move(move)
+        # TODO: submit lines to contract
         pass
 
     def _on_pick_color(self, color: str):
+        color_is_black = None
+        if color.upper() == "BLACK":
+            color_is_black = True
+        elif color.upper() == "WHITE":
+            color_is_black = False
+        else:
+            raise ValueError("Malformed color {}", color)
+        # TODO: submit to contract
         pass
 
     def _on_suggest(self, options: str):
-        pass
+        (line1, column1), (line2, column2) = \
+            map(self._parse_move, map(str.strip, options.split(' ')))
+        print(line1, column1, line2, column2)
+        # TODO: submit to contract
 
     def _on_select(self, move: str):
+        line, column = self._parse_move(move)
+        # TODO: submit to the contract
         pass
 
     def _on_pass(self, _: str):
@@ -100,5 +126,15 @@ class Repl:
             print("  * {} {}  -- {}".format(command, args, description))
 
     def _parse_move(self, move: str) -> (int, int):
-        if len(str) != 2:
-            raise ValueError("Invalid")
+        if len(move) != 2:
+            raise ValueError("Invalid MOVE format. It should be 2-characters long")
+        column = self._column_letters.get(move[0].upper(), None)
+        if column is None:
+            raise ValueError("Invalid MOVE format: unexpected column identifier: {}".format(move[0]))
+        line = int(move[1])
+        if not 1 <= line <= 15:
+            raise ValueError("Invalid MOVE format: unexpected line index {}. It is required to be in range [1, 15]")
+        return (15 - line), column
+
+if __name__ == "__main__":
+    Repl(None).handle_user_input("suggest B8 B5")
